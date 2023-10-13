1d6b63309db60ba4d5e31b8287ef0405b447cd62
harrisanth <93937172+harrisanth@users.noreply.github.com>
2023-10-13 18:59:41 -0400

    v0.0.0-11

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import graphql from '@rollup/plugin-graphql';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import replace from '@rollup/plugin-replace';
+// import replace from '@rollup/plugin-replace';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
@@ -15,48 +15,19 @@ export default [
 			{
 				file: 'dist/client.js',
 				format: 'cjs',
-				exports: 'named',
-				name: 'awoss_web',
 				sourcemap: true,
-				strict: false,
+				exports: 'named',
 				plugins: [filesize()],
 			},
 		],
 		plugins: [
 			peerDepsExternal(),
-			resolve({
-				preferBuiltins: false,
-				exportConditions: ['node'],
-				browser: true,
-				modulesOnly: true,
-			}),
-			commonjs({
-				include: /node_modules/,
-				//include: ['./index.js', 'node_modules/**'],
-				requireReturnsDefault: 'auto',
-			}),
-			typescript(),
+			resolve(),
+			json(),
 			graphql(),
-			replace({
-				'process.env.NODE_ENV': JSON.stringify('development'),
-				'process.env.REACT_APP_BASE_URL': '""', // src/client/framework/api.js
-				preventAssignment: true,
-			}),
-			// json(),
-
-			nodePolyfills(), //TODO: but why do node core modules wind up in the build anyways?
-		],
-		//external: [],
-		external: [
-			'react',
-			'react-dom',
-			'react-is',
-			'date-fns-tz',
-			'date-fns',
-			'date-fns/_lib/format/longFormatters',
-			'highlight.js',
-			'extend',
-			'style-to-object',
+			commonjs(),
+			typescript(),
+			nodePolyfills(),
 		],
 		onwarn(warning, warn) {
 			if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
