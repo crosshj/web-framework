e458c2325a8f862e8e4dbe69d9e8a2ed10671eb9
harrisanth <93937172+harrisanth@users.noreply.github.com>
2023-10-14 01:47:26 -0400

    parse graphql when sending request

--- src/client/framework/api.js
+++ src/client/framework/api.js
@@ -1,3 +1,29 @@
+import { parse } from 'graphql/language/parser.js';
+import { print } from 'graphql/language/printer.js';
+
+const extractOperationName = (document) => {
+	let operationName = undefined;
+	const operationDefinitions = document.definitions.filter(
+		(definition) => definition.kind === `OperationDefinition`,
+	);
+	if (operationDefinitions.length === 1) {
+		operationName = operationDefinitions[0]?.name?.value;
+	}
+	return operationName;
+};
+export const resolveRequestDocument = (document) => {
+	if (typeof document === `string`) {
+		let operationName = undefined;
+		try {
+			const parsedDocument = parse(document);
+			operationName = extractOperationName(parsedDocument);
+		} catch (err) {}
+		return { query: document, operationName };
+	}
+	const operationName = extractOperationName(document);
+	return { query: print(document), operationName };
+};
+
 const fetchAPI = async (url, options) => {
 	try {
 		const result = await fetch(url, options);
@@ -13,13 +39,15 @@ export const graphQLClient = {
 		const { tag, ...variablesRest } = variables || {};
 		const queryParams = typeof tag !== 'undefined' ? `?${tag}` : '';
 		const url = `/api/graphql${queryParams}`;
+		const { query, operationName } = resolveRequestDocument(args0);
 		const options = {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({
-				query: args0,
+				operationName,
+				query,
 				variables: variablesRest,
 				...argsRest,
 			}),
