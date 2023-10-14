641d17fc9c5200c041945eac4d8010d5e70326c7
harrisanth <93937172+harrisanth@users.noreply.github.com>
2023-10-13 22:43:20 -0400

    v0.0.0-18
    careful with svgHelpers
    github publish action +

--- src/client/global/utils/svgHelpers.js
+++ src/client/global/utils/svgHelpers.js
@@ -1,6 +1,8 @@
 export const InlineSrcSVG = (svgString) => {
+	const prefix = `data:image/svg+xml;base64,`;
+	if (!svgString) return prefix;
 	const buff = Buffer.from(svgString);
 	const base64data = buff.toString('base64');
-	const source = `data:image/svg+xml;base64,${base64data}`;
+	const source = `${prefix}${base64data}`;
 	return source;
 };
